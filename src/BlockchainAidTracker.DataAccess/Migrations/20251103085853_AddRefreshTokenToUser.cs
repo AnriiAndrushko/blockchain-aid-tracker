b75da405955c405ad2e0ed74cc36217bf47e0994
AndriiAndrushko <andry.i.andrushko@gmail.com>
2025-11-03 10:15:44 +0100

    Add `RefreshToken` and `ActualDeliveryDate` support to User and Shipment models
    Extend the `Users` table with `RefreshToken` and `RefreshTokenExpiresAt` fields for managing authentication token lifecycle. Add `ActualDeliveryDate` to the `Shipments` table for tracking delivery timelines. Update EF Core migrations and model snapshot accordingly.

---
+++ src/BlockchainAidTracker.DataAccess/Migrations/20251103085853_AddRefreshTokenToUser.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlockchainAidTracker.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiresAt",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ActualDeliveryDate",
+                table: "Shipments",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiresAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ActualDeliveryDate",
+                table: "Shipments");
+        }
+    }
+}
