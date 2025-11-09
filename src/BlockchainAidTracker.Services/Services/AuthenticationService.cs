35ef2ec96304ce38badc97a93ead5f9e1e37f0d4
AndriiAndrushko <andry.i.andrushko@gmail.com>
2025-11-09 14:00:11 +0100

    Add `FirstName` and `LastName` to access token generation in `AuthenticationService`

--- src/BlockchainAidTracker.Services/Services/AuthenticationService.cs
+++ src/BlockchainAidTracker.Services/Services/AuthenticationService.cs
@@ -219,7 +219,7 @@ public class AuthenticationService : IAuthenticationService
 
         // Generate new tokens
         var (accessToken, accessTokenExpiresAt) = _tokenService.GenerateAccessToken(
-            user.Id, user.Username, user.Email, user.Role.ToString());
+            user.Id, user.Username, user.Email, user.Role.ToString(), user.FirstName, user.LastName);
         var (refreshToken, refreshTokenExpiresAt) = _tokenService.GenerateRefreshToken();
 
         // Update user with new refresh token
